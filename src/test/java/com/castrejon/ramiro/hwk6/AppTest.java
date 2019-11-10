5c40a3f1a3550b70c00edf67b6af2b2d7a52302d
ramirocastrejon <47700941+ramirocastrejon@users.noreply.github.com>
2019-11-10 15:30:34 -0600

    Maven build of Triangle Class from Hwk 2 with Test coverage.

--- src/test/java/com/castrejon/ramiro/hwk6/AppTest.java
+++ src/test/java/com/castrejon/ramiro/hwk6/AppTest.java
@@ -1,5 +1,6 @@
 package com.castrejon.ramiro.hwk6;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,9 +13,49 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    App app = new App();
+    Triangle isos = new Triangle(5.0,5.0,8.0);
+    Triangle equi = new Triangle(5.0,5.0,5.0);
+    Triangle scaleRight = new Triangle(3.0,4.0,5.0);
+    Point origin = new Point();
+    Point test = new Point(3.0,4.0);
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void shouldBeValidTriangle(){
+        assertEquals(true, app.isItValid(3.0,4.0,5.0));
+
+    }
+    @Test
+    public void shouldBeNotValidTriangle(){
+        assertEquals(false, app.isItValid(3.0,5.0,8.0));
+    }
+
+    @Test
+    public void shouldBeIsosTriangle(){
+        assertEquals(true,isos.isIsosceles());
+    }
+    @Test
+    public void shouldBeEquilateralTriangle(){
+        assertEquals(true,equi.isEquilateral());
+    }
+    @Test
+    public void shouldBeScaleRightTriangle(){
+        assertEquals(true,scaleRight.isScalene());
+        assertEquals(true,scaleRight.isRight());
+    }
+    @Test
+    public void getArea(){
+        assertEquals(6.0,scaleRight.getArea(),0);
+        assertEquals(10.825,equi.getArea(),1);
+        assertEquals(12.0,isos.getArea(),0);
+    }
+    @Test
+    public void getDistanceTest(){
+        assertEquals(5.0,origin.distance(test),0);
     }
 }
